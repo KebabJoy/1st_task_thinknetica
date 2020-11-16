aea1c18f92ac61f185a3840a924b6bb226cb90ce
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-11-16 23:04:14 +0300

    PreFinal
    All classes are implemented

--- task.rb
+++ task.rb
@@ -1,7 +1,8 @@
 class Station
-    attr_reader :name; :trains
+    attr_reader :name; :trains 
     def initialize(name)
         @name = name
+        @trains = []
     end
     def trainsCount
         @trains.length
@@ -13,28 +14,109 @@ class Station
         @trains
     end
     def trainsByType(type)
-        return @trains.select {|train| train.type == type }
+        @trains.select {|train| train.type == type }                 
+    end
+    def departTrain(train)
+        if trains.include?(train)
+            trains.delete(train)
+        else
+            puts "Поезда нет на этой станции"
+        end
     end
 end
 
 class Route
-    attr_reader :start; :finish; :route
+    attr_reader :stations
     def initialize(start,finish)
-        @start = start
-        @finish = finish
+        @stations.unshift(start)
+        @stations << finish
     end
     def addStation(station)
-        @route << route
+        @stations.insert(stations.index(stations.last())-1,station)
+    end
+    def deleteStation(station)
+        @stations.delete_at(station)
+    end
+    def printStations
+        stations.each do |station|
+            puts station
+        end
     end
 end
 
 
 class Train
-    attr_reader :type; :number; :wagonsCount
-    def initialize(type, num, wagons)
+    attr_accessor :speed
+    attr_reader :type; :number; :wagonsCount; :route; :currentStation; :direction
+    def initialize(type, num, wagons,route)
         @type = type
         @number = num
         @wagonsCount = wagons
+        @route = route
+        @speed = 0
+        @currentStation = 0
+        @direction = true # true -> forward. false -> backwards
+        @route.stations[0].trains << self
     end
-    
+    def wagonsCount
+        @wagonsCount
+    end
+    def currentSpeed
+        @speed
+    end
+    def addWagon
+        if @speed == 0
+            @wagonsCount += 1
+        end
+    end
+    def deleteWagon
+        if @speed == 0
+            @wagonsCount -= 1
+        end
+    end
+    def nextStation
+        @route.stations[currentStation].trains.delete(self)
+        self.move
+        if currentStation == route.stations.length()-1 
+           @direction = !@direction 
+        end
+        if @direction
+            @currentStation += 1
+           
+        else
+            @currentStation -= 1
+        end
+        @route.stations[currentStation] << self
+        if currentStation == 0
+            @direction = !@direction
+        end
+        self.stop
+    end
+    def move
+        @speed = 80
+    end
+    def stop
+        @speed = 0
+    end
+    def currentStation
+        @currentStation
+    end
+    def nextStation
+        if @currentStation == route.stations.length()-1 
+            @route.stations[@currentStation-1]
+        else
+            @route.stations[@currentStation+1]
+        end
+    end
+    def previousStation
+        if @currentStation == 0
+            @route.stations[@currentStation+1]
+        else
+            @route.stations[@currentStation-1]
+        end
+    end
 end
+
+
+
+
